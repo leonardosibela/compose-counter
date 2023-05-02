b5ef21a4cd67cc34bb6b3ed3c968b6bd78850f2f
Leonardo Sibela <leonardo_sibela@hotmail.com>
2023-05-02 15:18:22 -0300

    Initial commit

---
+++ app/src/main/java/com/honkgoose/composecounter/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.honkgoose.composecounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.honkgoose.composecounter.ui.theme.ComposeCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCounterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Hello world")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String) {
+    Text(text = text)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCounterTheme {
+        Greeting("Hello world")
+    }
+}
